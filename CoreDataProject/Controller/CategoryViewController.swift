ca9a5bc84f5de5e2bb1f3b6fcf565d149869950b
Dhruva9891 <dhruvabeti@gmail.com>
2021-03-09 11:35:38 +0530

    Initial project setup

---
+++ CoreDataProject/Controller/CategoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  CoreDataProject
+//
+//  Created by Dhruva Beti on 08/03/21.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController {
+    
+    @IBOutlet weak var categoryTableview: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func savePressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func updatePressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func deletePressed(_ sender: UIButton) {
+    }
+}
+
---
+++ CoreDataProject/Controller/ItemViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ItemViewController.swift
+//  CoreDataProject
+//
+//  Created by Dhruva Beti on 08/03/21.
+//
+
+import UIKit
+
+class ItemViewController: UIViewController {
+    
+    @IBOutlet weak var itemTableview: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func savePressed(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func updatePressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func deletePressed(_ sender: UIButton) {
+    }
+}
+
